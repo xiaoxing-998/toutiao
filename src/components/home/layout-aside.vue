bbf11b76f3ddce09a5b02347dd34e9f65e04127f
unknown <sstar>
2019-12-19 21:09:02 +0800

    主页模块-左侧导航菜单

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="navAside">
+    <div class="logo">
+      <img src="../../assets/img/logo_admin.png" alt="logo" />
+    </div>
+    <el-menu background-color="#353b4e" text-color="#adafb5" active-text-color="#409eff">
+      <el-menu-item>首页</el-menu-item>
+      <el-submenu index="1">
+        <span slot="title">内容管理</span>
+        <el-menu-item-group>
+          <el-menu-item>发布文章</el-menu-item>
+          <el-menu-item>内容列表</el-menu-item>
+          <el-menu-item>评论列表</el-menu-item>
+          <el-menu-item>素材管理</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="2">
+        <span slot="title">粉丝管理</span>
+        <el-menu-item-group>
+          <el-menu-item>图文数据</el-menu-item>
+          <el-menu-item>粉丝概况</el-menu-item>
+          <el-menu-item>粉丝画像</el-menu-item>
+          <el-menu-item>粉丝列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-menu-item>账户信息</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.navAside {
+    width: 201px;
+    .logo {
+        text-align: center;
+        background-color: #2e2f32;
+        padding:10px;
+        img {
+            height: 30px;
+        }
+    }
+}
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>主页</div>
+  <el-container>
+    <!-- 左侧导航 -->
+    <el-aside width="200px" class="navaside">
+      <navAside></navAside>
+    </el-aside>
+    <!-- 中间内容容器 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header>头部</el-header>
+      <!-- 二级视图 -->
+      <el-main>主要区域</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import navAside from '../../components/home/layout-aside'
 export default {
-
+  // 局部注册组件
+  components: {
+    navAside
+  }
 }
 </script>
 
-<style>
+<style  lang="less" scoped>
+.navaside {
+  background-color: #353b4e;
+  height: 100vh;
+  overflow: hidden;
 
+}
 </style>
